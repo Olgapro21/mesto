6df41583ebf926b5130cea01141ed1eba28f0aa1
Olga Provb9+ <ola21@ya.ru>
2023-03-13 22:49:08 +0300

    Внесла изменения согласно замечаниям ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-const popupOpenButtonElement = document.querySelector(".profile-bloks__edit-button");
+const popupOpenButtonElement = document.querySelector(".profile__edit-button");
 
 const openPopup = function () {
   popupElement.classList.add("popup_opened");
   nameInputElement.value = profileNameElement.textContent;
   jobInputElement.value =profileJobElement.textContent;
-  console.log("Open popup clicked");
 };
 
 const closePopup = function () {
@@ -20,15 +19,15 @@ popupCloseButtonElement.addEventListener("click", closePopup);
 let formElement = popupElement.querySelector(".popup__form");
 let nameInputElement = formElement.querySelector(".popup__form-input_status_name");
 let jobInputElement = formElement.querySelector(".popup__form-input_status_job");
-let profileNameElement = document.querySelector(".profile-bloks__title");
-let profileJobElement = document.querySelector(".profile-bloks__subtitle");
+let profileNameElement = document.querySelector(".profile__title");
+let profileJobElement = document.querySelector(".profile__subtitle");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileNameElement.textContent = nameInputElement.value;
   profileJobElement.textContent = jobInputElement.value;
 
-    popupElement.classList.remove("popup_opened");
+    popupElement.classList.remove("closePopup");
 };
 
 formElement.addEventListener('submit', handleFormSubmit);
