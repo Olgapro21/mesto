15df3d35d56fa0ad50cd4ee7a18658889ef08ddc
Olga Provb9+ <ola21@ya.ru>
2023-03-21 18:53:58 +0300

    Добавила картачки на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -33,6 +33,50 @@ function handleFormSubmit(evt) {
 formElement.addEventListener('submit', handleFormSubmit);
 
 //5 спринт
+// 1. Шесть карточек «из коробки»
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const itemTemplate = document.querySelector(".elements-template").content;
+const list = document.querySelector(".element");
+const formInputtitle = document.querySelector(".popup__form-input_type_title");
+const formInputlink = document.querySelector(".popup__form-input_type_link");
+const formButton = document.querySelector(".popup__save_type_card");
+
+initialCards.forEach(renderItem);
+
+function renderItem(item) {
+  const htmlElement = itemTemplate.cloneNode(true);
+  htmlElement.querySelector(".element__image").src = item.link;
+  htmlElement.querySelector(".element__image").alt = item.name;
+  htmlElement.querySelector(".element__title").textContent = item.name;
+  list.append(htmlElement);
+}
+
+//2. Форма добавления карточки
 const popupCardElement = document.querySelector(".popup_type_card");
 const popupCardOpenButtonElement = document.querySelector(".profile__add-button"); //открыть
 const popupCardCloseButtonElement = document.querySelector(".popup__close-card"); //закрыть
@@ -48,4 +92,37 @@ const closePopupCard  = function () {
 
 popupCardOpenButtonElement.addEventListener("click", openPopupCard);
 popupCardCloseButtonElement.addEventListener("click", closePopupCard);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
