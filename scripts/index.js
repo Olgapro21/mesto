22f6e20cfbcbcd05631dc45edf319fb451850624
Olga Provb9+ <ola21@ya.ru>
2023-03-27 14:42:49 +0300

    Внесла правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,46 @@
 //4 спринт
-const popup = document.querySelector('.popup'); // основной попап
-const popupElement = document.querySelector(".popup_type_profile"); // доп попап профиля
-const popupCloseButtonElements = document.querySelectorAll(".popup__close"); // кнопка закрытия попапа
-const popupOpenButtonElement = document.querySelector(".profile__edit-button"); // кнопка открытия профиля
+const profilePopup = document.querySelector(".popup_type_profile"); // доп попап профиля
+const popupCloseButton = document.querySelectorAll(".popup__close"); // кнопка закрытия попапа
+const profileOpenButton = document.querySelector(".profile__edit-button"); // кнопка открытия профиля
 
-//открытие попапа профиля
-const openPopup = function () {
-  popupElement.classList.add("popup_opened");
-  nameInputElement.value = profileNameElement.textContent;
-  jobInputElement.value =profileJobElement.textContent;
+//открытие попапов
+function openPopup (popup) {
+  popup.classList.add("popup_opened");
 };
 
-//закрытие попапа профиля
+profileOpenButton.addEventListener("click", () => {
+  nameInputProfileForm.value = profileNameForm.textContent;
+  jobInputProfileForm.value = profileJobForm.textContent;
+  openPopup(profilePopup)
+});
+
+//закрытие попапов
 function closePopup (popup) {
   popup.classList.remove("popup_opened");
 };
 
-popupCloseButtonElements.forEach((element) => {
+popupCloseButton.forEach((element) => {
   const popup = element.closest(".popup");
   element.addEventListener("click", () => {
     closePopup (popup)
   })
 });
 
-popupOpenButtonElement.addEventListener("click", openPopup);
-
 // Меняем статус аватара
-let formElement = popupElement.querySelector(".popup__form");
-let nameInputElement = formElement.querySelector(".popup__form-input_status_name");
-let jobInputElement = formElement.querySelector(".popup__form-input_status_job");
-let profileNameElement = document.querySelector(".profile__title");
-let profileJobElement = document.querySelector(".profile__subtitle");
+const profileForm = profilePopup.querySelector(".popup__form"); //форма профиля
+const nameInputProfileForm = profileForm.querySelector(".popup__form-input_status_name");
+const jobInputProfileForm = profileForm.querySelector(".popup__form-input_status_job");
+const profileNameForm = document.querySelector(".profile__title");
+const profileJobForm = document.querySelector(".profile__subtitle");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  profileNameElement.textContent = nameInputElement.value;
-  profileJobElement.textContent = jobInputElement.value;
-  closePopup(popupElement);
+  profileNameForm.textContent = nameInputProfileForm.value;
+  profileJobForm.textContent = jobInputProfileForm.value;
+  closePopup(profilePopup);
 };
 
-formElement.addEventListener('submit', handleFormSubmit);
+profileForm.addEventListener('submit', handleFormSubmit);
 
 //5 спринт
 // Шесть карточек «из коробки»
@@ -72,38 +73,32 @@ const initialCards = [
 
 const itemTemplate = document.querySelector(".elements-template").content; // нашла темлейт
 const list = document.querySelector(".element"); // нашла ul
-const formButton = document.querySelector(".popup__save_type_card");
 const popupImageElement = document.querySelector(".popup_type_image"); //доп попап открытие фото
-const popupImageCloseButtonElement = document.querySelector(".popup__close-image"); //закрыть
 const popupImage = document.querySelector(".popup__image");
 const popuptitle = document.querySelector(".popup__image-title");
 
 //Создаем массив карточек
 function renderItem(item) {
   const htmlElement = itemTemplate.cloneNode(true); // скопировала темплейт
-  htmlElement.querySelector(".element__image").src = item.link; //добавила картинки
-  htmlElement.querySelector(".element__image").alt = item.name;
+  const imageTemplateElement = htmlElement.querySelector(".element__image");
+  imageTemplateElement.src = item.link;
+  imageTemplateElement.alt = item.link;
   htmlElement.querySelector(".element__title").textContent = item.name; //добавила текст
-  htmlElement.querySelector('.element__image').addEventListener('click', function () {
+  imageTemplateElement.addEventListener('click', function () {
   popupImage.src = item.link;
   popupImage.alt = item.name;
   popuptitle.textContent = item.name;
-  openPopupImage ();
+  openPopup(popupImageElement);
   });
   setEventListeners(htmlElement); //вызвала функцию
   return htmlElement
 };
 
 //Прошлась по массиву
-initialCards.forEach((Object) => {
-const card = renderItem(Object);
+initialCards.forEach((object) => {
+const card = renderItem(object);
 list.append(card); //добавила с помощью метода append наш клон в ul в конец
 });
-
-//открытие попапа просмотр фото
-const openPopupImage = function () {
-  popupImageElement.classList.add("popup_opened");
-}
 
 // Удаление карточки
 function handleDelete (event) {
@@ -126,18 +121,15 @@ function setEventListeners(htmlElement) {
 //Форма открытия попапа "Новое место"
 const popupCardElement = document.querySelector(".popup_type_card"); //доп попап Новое место
 const popupCardOpenButtonElement = document.querySelector(".profile__add-button"); //открыть
-const popupCardCloseButtonElement = document.querySelector(".popup__close-card"); //закрыть
 const formCardElement = popupCardElement.querySelector(".popup__form");
 const titleElement = document.querySelector(".element__title");
 const formInputtitle = document.querySelector(".popup__form-input_type_title"); //импут надписи
 const formInputlink = document.querySelector(".popup__form-input_type_link"); //импут фото
 
-//Открытие
-const openPopupCard = function () {
-  popupCardElement.classList.add("popup_opened"); //добавила popup_opened
-};
-
-popupCardOpenButtonElement.addEventListener("click", openPopupCard);
+//Открытие попапа "Новое место"
+popupCardOpenButtonElement.addEventListener("click", () => {
+  openPopup(popupCardElement)
+});
 
 // Сабмит передачи новых данных в массив
 formCardElement.addEventListener("submit", (evt) => {
