adb64727148acd53fac704eed1d686f7ee84c21e
Olga Provb9+ <ola21@ya.ru>
2023-03-28 14:32:39 +0300

    Внесла последние правки

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,9 @@ const popupCloseButton = document.querySelectorAll(".popup__close"); // кноп
 const profileOpenButton = document.querySelector(".profile__edit-button"); // кнопка открытия профиля
 
 //открытие попапов
-function openPopup (popup) {
+function openPopup(popup) {
   popup.classList.add("popup_opened");
-};
+}
 
 profileOpenButton.addEventListener("click", () => {
   nameInputProfileForm.value = profileNameForm.textContent;
@@ -15,15 +15,15 @@ profileOpenButton.addEventListener("click", () => {
 });
 
 //закрытие попапов
-function closePopup (popup) {
+function closePopup(popup) {
   popup.classList.remove("popup_opened");
-};
+}
 
 popupCloseButton.forEach((element) => {
   const popup = element.closest(".popup");
   element.addEventListener("click", () => {
-    closePopup (popup);
-  })
+    closePopup(popup);
+  });
 });
 
 // Меняем статус аватара
@@ -33,42 +33,42 @@ const jobInputProfileForm = profileForm.querySelector(".popup__form-input_status
 const profileNameForm = document.querySelector(".profile__title");
 const profileJobForm = document.querySelector(".profile__subtitle");
 
-function handleFormProfileSubmit(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileNameForm.textContent = nameInputProfileForm.value;
   profileJobForm.textContent = jobInputProfileForm.value;
   closePopup(profilePopup);
-};
+}
 
-profileForm.addEventListener('submit', handleFormProfileSubmit);
+profileForm.addEventListener("submit", handleProfileFormSubmit);
 
 //5 спринт
 // Шесть карточек «из коробки»
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
 const itemTemplate = document.querySelector(".elements-template").content; // нашла темлейт
@@ -84,15 +84,15 @@ function renderItem(item) {
   imageTemplateElement.src = item.link;
   imageTemplateElement.alt = item.link;
   htmlElement.querySelector(".element__title").textContent = item.name;
-  imageTemplateElement.addEventListener('click', function () {
+  imageTemplateElement.addEventListener("click", function () {
     popupImage.src = item.link;
     popupImage.alt = item.name;
     popuptitle.textContent = item.name;
     openPopup(popupImageElement);
   });
   setEventListeners(htmlElement); //вызвала функцию
-  return htmlElement
-};
+  return htmlElement;
+}
 
 //Прошлась по массиву
 initialCards.forEach((object) => {
@@ -101,22 +101,22 @@ initialCards.forEach((object) => {
 });
 
 // Удаление карточки
-function handleDelete (event) {
+function handleDelete(event) {
   const listDelete = event.target.closest(".element__list");
   listDelete.remove(); // удалила DOM элемент
-};
+}
 
 // Лайк карточки
-function handleLike (evt) {
+function handleLike(evt) {
   const likeActive = evt.target;
   evt.target.classList.toggle("element__like-ico_active");
-};
+}
 
 //создала отдельную функцию, чтобы не загружать темплейт
 function setEventListeners(htmlElement) {
-	htmlElement.querySelector(".element__delete-image").addEventListener('click', handleDelete); //добавила кнопку карзины на которую про
-  htmlElement.querySelector(".element__like-ico").addEventListener('click', handleLike); // лайк
-};
+  htmlElement.querySelector(".element__delete-image").addEventListener("click", handleDelete); //добавила кнопку карзины на которую про
+  htmlElement.querySelector(".element__like-ico").addEventListener("click", handleLike); // лайк
+}
 
 //Форма открытия попапа "Новое место"
 const popupCardElement = document.querySelector(".popup_type_card"); //доп попап Новое место
@@ -128,10 +128,10 @@ const formInputlink = document.querySelector(".popup__form-input_type_link"); //
 
 //Открытие попапа "Новое место"
 popupCardOpenButtonElement.addEventListener("click", () => {
-  openPopup(popupCardElement)
+  openPopup(popupCardElement);
 });
 
-// Сабмит передачи новых данных в массив
+//Сабмит передачи новых данных в массив
 formCardElement.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const itemNew = {name: formInputtitle.value, link: formInputlink.value};
